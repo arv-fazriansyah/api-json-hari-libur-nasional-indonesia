3b301d83f8f3731c5cf734c364ed4615e7f6409d
Arvib Fazriansyah <85182275+arv-fazriansyah@users.noreply.github.com>
2023-03-24 16:41:00 +0700

    Update gsheet.js

--- gsheet.js
+++ gsheet.js
@@ -2,39 +2,86 @@
   var url = "https://script.google.com/macros/s/AKfycbyKaES3UrD2_gNOYV-TSZsKxbLIFp2BkHfmZ3Uy94JWOmphrIsM4ASEOiKTjAxZzJanXg/exec";
 
 
-  fetch(url)
-    .then(function(response) {
-      return response.json();
-    })
-    .then(function(data) {
-      var tableHeader = document.getElementById("tableHeader");
-      var tableBody = document.getElementById("tableBody");
+  var originalData = []; // To store the original data from JSON
+var filteredData = []; // To store the filtered data
+var pageSize = 10; // The number of rows to display per page
+var currentPage = 0; // The index of the current page
+var startRow = 0; // The index of the first row to display
+var endRow = pageSize; // The index of the last row to display
 
+fetch(url)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
+    originalData = data; // Store the original data for filtering later
+    createTableHeader(); // Create the table header dynamically
+    filterTable(); // Filter the table initially
+  })
+  .catch(function(error) {
+    console.error(error);
+  });
 
-      // Create header row dynamically based on the keys in the first object
-      var headerRow = document.createElement("tr");
-      var keys = Object.keys(data[0]);
-      for (var i = 0; i < keys.length; i++) {
-        var key = keys[i];
-        var th = document.createElement("th");
-        th.appendChild(document.createTextNode(key));
-        headerRow.appendChild(th);
-      }
-      tableHeader.appendChild(headerRow);
+// Function to create the table header dynamically
+function createTableHeader() {
+  var tableHeader = document.getElementById("tableHeader");
+  var headerRow = document.createElement("tr");
+  var keys = Object.keys(originalData[0]);
+  for (var i = 0; i < keys.length; i++) {
+    var cell = document.createElement("th");
+    cell.appendChild(document.createTextNode(keys[i]));
+    headerRow.appendChild(cell);
+  }
+  tableHeader.appendChild(headerRow);
+}
 
-
-      // Create data rows dynamically
-      for (var i = 0; i < data.length; i++) {
-        var row = document.createElement("tr");
-        for (var j = 0; j < keys.length; j++) {
-          var key = keys[j];
-          var cell = document.createElement("td");
-          cell.appendChild(document.createTextNode(data[i][key]));
-          row.appendChild(cell);
-        }
-        tableBody.appendChild(row);
-      }
-    })
-    .catch(function(error) {
-      console.error(error);
+// Function to filter the table based on search input and current page
+function filterTable() {
+  var input = document.getElementById("searchInput").value.toLowerCase();
+  filteredData = originalData.filter(function(obj) {
+    return Object.keys(obj).some(function(key) {
+      return obj[key].toString().toLowerCase().includes(input);
     });
+  });
+
+  // Update the start and end rows based on the current page
+  startRow = currentPage * pageSize;
+  endRow = Math.min(startRow + pageSize, filteredData.length);
+
+  var tableBody = document.getElementById("tableBody");
+  tableBody.innerHTML = "";
+
+  // Create data rows dynamically for the current page
+  for (var i = startRow; i < endRow; i++) {
+    var row = document.createElement("tr");
+    for (var j = 0; j < Object.keys(filteredData[i]).length; j++) {
+      var key = Object.keys(filteredData[i])[j];
+      var cell = document.createElement("td");
+      cell.appendChild(document.createTextNode(filteredData[i][key]));
+      row.appendChild(cell);
+    }
+    tableBody.appendChild(row);
+  }
+
+  // Disable or enable the "Previous" and "Next" buttons based on the current page
+  var prevBtn = document.getElementById("prevBtn");
+  var nextBtn = document.getElementById("nextBtn");
+  prevBtn.disabled = (currentPage === 0);
+  nextBtn.disabled = (endRow >= filteredData.length);
+}
+
+// Function to go to the previous page
+function prevPage() {
+  if (currentPage > 0) {
+    currentPage--;
+    filterTable();
+  }
+}
+
+// Function to go to the next page
+function nextPage() {
+  if (endRow < filteredData.length) {
+   currentPage++;
+   filterTable();
+  }
+}
