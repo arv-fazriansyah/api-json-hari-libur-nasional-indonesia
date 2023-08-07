aad608f8e776ec6d11742aaa826eefea24c5118a
Arvib Fazriansyah <85182275+arv-fazriansyah@users.noreply.github.com>
2023-08-07 20:11:06 +0700

    Update gsheet.js

--- gsheet.js
+++ gsheet.js
@@ -1,7 +1,7 @@
 // Replace this URL with the URL for your own JSON data
 // var url = "https://api.fazriansyah.eu.org/";
 // var url = "https://cors-anywhere.herokuapp.com/https://api.fazriansyah.eu.org/";
-var url = "https://crossorigin.me/https://api.fazriansyah.eu.org/";
+var url = "https://api.codetabs.com/v1/proxy/?quest=https://api.fazriansyah.eu.org/";
 
 
 var originalData = []; // To store the original data from JSON
